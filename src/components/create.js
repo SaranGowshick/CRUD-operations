20ff320cc06d78637ac20470055b1163073ae523
Gowshick <70029396+SaranGowshick@users.noreply.github.com>
2023-09-24 02:18:34 +0530

    Update create.js

--- src/components/create.js
+++ src/components/create.js
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import {Link } from 'react-router-dom';
-import './css/index.css';
+
 
 const api_base="http://localhost:3001";
 function Create() {
