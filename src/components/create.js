78e576fe18614856a90f82b6e186b535af98f7ae
Gowshick <70029396+SaranGowshick@users.noreply.github.com>
2023-09-25 00:33:18 +0530

    Update create.js

--- src/components/create.js
+++ src/components/create.js
@@ -1,8 +1,8 @@
 import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import {Link } from 'react-router-dom';
-
-
+import './css/index.css';
+import logo from './assets/logo.png'
 const api_base="http://localhost:3001";
 function Create() {
   const[employeeName,setEmployeename]=useState('');
@@ -19,17 +19,20 @@ function Create() {
     }
     if(employeeID.length>6){
       alert("Id Should not greater Than 6")
+      setEmployeeID('');
       error.employeeID = 'Id error';
     }
     if(employeeEmail.trim() === ''){
       alert("Missing Email Field");
-      errors.employeeEmail = 'Missing Email Field';
+      error.employeeEmail = 'Missing Email Field';
     }else if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(employeeEmail)) {
       alert("Wrong Email ID")
-      errors.employeeEmail = 'Invalid email address';
+      setEmployeeEmail('');
+      error.employeeEmail = 'Invalid email address';
     }
-    if(employeePhone.length>10){
-      alert("Phone Number Should Not Be Greater Than 10 Numbers");
+    if(employeePhone.length != 10){
+      alert("Phone Number Should be atleast 10 Numbers");
+      setEmployeePhone('');
       error.employeePhone='Invalid Phone Number';
     }
     setErrors(error);
@@ -44,7 +47,8 @@ function Create() {
           EmployeeName: employeeName,
           EmployeeID: employeeID,
           EmployeeEmail:employeeEmail,
-          EmployeePhone:employeePhone
+          EmployeePhone:employeePhone,
+          Error:errors
         }).then((res)=>{
           console.log("response")
           if(res.status===201){
@@ -52,17 +56,19 @@ function Create() {
             setEmployeename('');
             setEmployeeID('');
             setEmployeeEmail('');
-          }else if(res.status===400){
-            console.log("hello")
-            alert("Missing Form Data");
+            setEmployeePhone('');
           }
         })
       }else{
         const error={};
         setErrors(error);
+
       }
     }catch(error){
-      console.log('Error submitting employee:', error);
+     if(error.status===400){
+      console.log("hii");
+      alert("EMployee Data Is Invalid")
+     }
     }
   }
   
@@ -70,38 +76,59 @@ function Create() {
 
   
   return (
+    <div className='employ'>
+         <header className='employee-dashboard'>
+                <div className='h1-text'>
+                    <div>
+                    <h1 id='dashboard-text'>Employee Dashboard</h1>
+                    </div>
+                </div>
+                <div>
+                    <img src={logo} alt='logo'/>
+                </div>
+            </header>
+      
     <div className='employee'>
-        <header>
-          <h1>Enter Employee Details</h1>
-        </header>
         <div className='employee-form'>
-            <div className='employee-name'>
-              <label htmlFor='employee-name'>EmployeeName:</label>
-              <input type='text'onChange={(e)=>setEmployeename(e.target.value)} required={true}/>
+            
+              <div className="employee-align">
+            
+                
+                <div className='text'>
+                  <h3><b>Register New Employee</b></h3>
+                </div>
+              <div className='employee-name'>
+                {/* <label htmlFor='employee-name'>EmployeeName:</label><br/> */}
+                <input type='text'onChange={(e)=>setEmployeename(e.target.value)} placeholder='Employee Name' required={true} value={employeeName}/>
+              </div>
+            
+              <div className='employee-id'>
+                {/* <label htmlFor='employee-id'>EmployeeID:</label><br/> */}
+                <input type='text'onChange={(e)=>setEmployeeID(e.target.value)}placeholder='EmployeeID'required={true} value={employeeID}/>
+              </div>
+            
+              <div className='employee-email'>
+                {/* <label htmlFor='employee-email'>Email.ID:</label><br/> */}
+                <input type='text'onChange={(e)=>setEmployeeEmail(e.target.value)}placeholder='Employee Email'required={true} value={employeeEmail}/>
+              </div>
+            
+              <div className='employee-phone'>
+                {/* <label htmlFor='employee-phone'>Phone.Number:</label><br/> */}
+                <input type='tel'onChange={(e)=>setEmployeePhone(e.target.value)}placeholder='Employee Phone'required={true} value={employeePhone}/>
+              </div>
+              
+              <div className='button' >
+               
+                  <button id='btn'><Link className="link" to="/">Back</Link></button>
+                  <button className='btn-submit'id='btn'onClick={Submit}>Submit</button>
+                
+              </div>  
             </div>
-            <br/>
-            <div className='employee-id'>
-              <label htmlFor='employee-id'>EmployeeID:</label>
-              <input type='text'onChange={(e)=>setEmployeeID(e.target.value)}required={true}/>
+             
             </div>
-            <br/>
-            <div className='employee-email'>
-              <label htmlFor='employee-email'>Email.ID:</label>
-              <input type='text'onChange={(e)=>setEmployeeEmail(e.target.value)}required={true}/>
-            </div>
-            <br/>
-            <div className='employee-phone'>
-              <label htmlFor='employee-phone'>Phone.Number:</label>
-              <input type='tel'onChange={(e)=>setEmployeePhone(e.target.value)}required={true}/>
-            </div>
-            <br/>
-            <div className='submit-button'>
-              <button onClick={Submit}>Submit</button>
-            </div>
-        </div>
-        <br/>
-        <button><Link to="/read">Read</Link>
-        </button>
+        
+       
+            </div>  
     </div>
   );
 }
