c266b209d2c0cb5bdd1f86cfc465393535a04bc3
Gowshick <70029396+SaranGowshick@users.noreply.github.com>
2023-09-25 00:34:02 +0530

    Update read.js

--- src/components/read.js
+++ src/components/read.js
@@ -1,12 +1,12 @@
 import React, { useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Update from './update';
+import './css/index.css';
+import logo from './assets/logo.png';
 
 const api_base="http://localhost:3001";
 function Read() {
     const[employees,setEmployees]=useState([]);
-    const[click,setClick]=useState(false);
-  
+    
     useEffect(()=>{
         GetEmployee();
     },[]);
@@ -17,61 +17,76 @@ function Read() {
             .then((data)=>setEmployees(data))
             .catch((error)=>console.log(error))
     }
-
-    const HandleOnClick=()=>{
-      
-      setClick(true);
-      
-    }
-    const employeeDelete= async id => {
-      
-      const data = await fetch(api_base + '/deleteemployee/' + id, { method: "DELETE" }).then(res => res.json());
+   
   
-      setEmployees(employees=> employees.filter(employees => employees._id !== data.result._id));
-      
-    }
+    const employeeDelete= async id => {
+    
+
+      alert("This Process Can't Be Undone ");
+     
+        const data = await fetch(api_base + '/deleteemployee/' + id, { method: "DELETE" }).then(res => res.json());
+  
+        setEmployees(employees=> employees.filter(employees => employees._id !== data.result._id));
+      }
+    
    
    
   const filteredKeys = Object.keys(employees[0] || {}).filter((key) => key !== '_id' && key !== '__v');
   return (
-    <div className='employee'>
-        <div className='employee-read'>
-          <button><Link to='/'>Create</Link></button>
-          <button onClick={HandleOnClick}>Show</button>
-          <div>
-          <table>
-                    <thead>
-                        <tr>
-                           {filteredKeys && filteredKeys.length>0 &&
-                            filteredKeys.map((key)=>(
-                                <th key={key}>{key}</th>
-                            ))
-                            }
-                            <th>Update</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                      {employees && employees.length > 0 &&
-                                employees.map((item) => (
-                                    <tr key={item._id}>
-                                        <td>{item.Employeename}</td>
-                                        <td>{item.EmployeeID}</td>
-                                        <td>{item.Email}</td>
-                                        <td>{item.Phone}</td>
-                                        <td><button><Link to={`/update/${item._id}`}>update</Link></button></td>
-                                        <td><button onClick={() => employeeDelete(item._id)}>delete</button></td>
-                                    </tr>
-                                ))
-                            }
-                    </tbody>
-                </table>
-                 
-                 
+    <div className='employee-read'>
+        <header className='employee-dashboard'>
+                <div className='h1-text'>
+                    <div>
+                    <h1 id='dashboard-text'>Employee Dashboard</h1>
+                    </div>
+                </div>
+                <div>
+                    <img src={logo} alt='logo'/>
+                </div>
+            </header>
+        <div className='employee-display'>
+         
+          <div className='employee-table'>
+            <br/>
+            <div className='employee-db'>
+                <h1>Employees List</h1>
+                  <button class='btn-create'id='btn3'><Link className='link'to='/create'>Add Employee</Link></button>
+            </div>
+            <br/>
+            <table id="customers">
+              <thead>
+                <tr>
+                  <th>S.No</th>
+                  <th>Employee Name</th>
+                  <th>Employee ID</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Update</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {employees && employees.length > 0 &&
+                  employees.map((item,index) => (
+                  <tr key={item._id}>
+                        <td>{index+1}</td>
+                        <td>{item.Employeename}</td>
+                        <td>{item.EmployeeID}</td>
+                        <td>{item.Email}</td>
+                        <td>{item.Phone}</td>
+                        <td><button id='btn'><Link className="link" to={`/update/${item._id}`}>update</Link></button></td>
+                        <td><button id='btn2' onClick={() => employeeDelete(item._id)}>delete</button></td>
+                  </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+            <br/>
+            
           </div>
+          
         </div>
-        
-    </div>
+      </div>
   );
 }
 
